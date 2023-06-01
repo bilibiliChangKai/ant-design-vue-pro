1e15321f15aab1ef49dc10fe248cc193d5b3095d
bilibiliChangKai <3254266353@qq.com>
2023-06-01 23:09:47 +0800

    feat: 添加两个临时页面

---
+++ src/views/other/TestImage.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <a-card title="图片收藏">
+      <div class="image-gallery">
+        <div class="image-gallery__display">
+          <img :src="displayImage" alt="展示区" />
+        </div>
+        <div class="image-gallery__collection">
+          <a-row :gutter="16">
+            <a-col :span="2" v-for="(image, index) in collection" :key="index">
+              <a-card :hoverable="true" class="thumbnail-card" @click="handleThumbnailClick(index)">
+                <img :src="image.thumbnail" alt="缩略图" />
+              </a-card>
+            </a-col>
+          </a-row>
+        </div>
+      </div>
+    </a-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        collection: [
+        {
+            thumbnail: 'https://i.pinimg.com/474x/6b/91/43/6b91435bd6c0a1da64d293e34b0a9d13.jpg',
+            image: 'https://i.pinimg.com/474x/6b/91/43/6b91435bd6c0a1da64d293e34b0a9d13.jpg'
+          },
+          {
+            thumbnail: 'https://img2.woyaogexing.com/2021/08/17/d686699abe2247909206e921c7eae3bb!400x400.png',
+            image: 'https://img2.woyaogexing.com/2021/08/17/d686699abe2247909206e921c7eae3bb!400x400.png'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          },
+          {
+            thumbnail: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg',
+            image: 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+          }
+        ],
+        displayImage: ''
+      }
+    },
+    methods: {
+      handleThumbnailClick (index) {
+        this.displayImage = this.collection[index].image
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.image-gallery {
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+}
+
+.image-gallery__display {
+width: 512px;
+height: 512px;
+margin-bottom: 16px;
+}
+
+/* .image-gallery__collection {
+width: 100%;
+}
+
+.thumbnail-card {
+border: 1px solid #d9d9d9;
+border-radius: 2px;
+width: 100%;
+height: 0;
+padding-bottom: 100%;
+overflow: hidden;
+}
+
+.thumbnail-card img {
+width: 100%;
+height: 100%;
+object-fit: cover;
+} */
+
+.image-gallery__collection {
+display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.thumbnail-card {
+border: 1px solid #d9d9d9;
+border-radius: 2px;
+width: 100%;
+height: 0;
+padding-bottom: 100%;
+overflow: hidden;
+display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.thumbnail-card img {
+width: 100%;
+height: 100%;
+object-fit: cover;
+}
+</style>
---
+++ src/views/other/TestPage.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <a-card title="文生图功能">
+      <div>选择模型</div>
+      <a-select v-model="selectedModel" style="width: 400px; margin-top: 10px">
+        <a-select-option
+          v-for="model in models"
+          :key="model.value"
+          :value="model.value"
+        >
+          {{ model.label }}
+        </a-select-option>
+      </a-select>
+
+      <div style="margin-top: 20px">选择子模型</div>
+      <a-select v-model="selectedSubModel" style="width: 400px; margin-top: 10px">
+        <a-select-option
+          v-for="subModel in subModels"
+          :key="subModel.value"
+          :value="subModel.value"
+        >
+          {{ subModel.label }}
+        </a-select-option>
+      </a-select>
+
+      <div style="margin-top: 20px">关键字输入</div>
+      <a-input
+        v-model="keyword"
+        placeholder="请输入关键字"
+        style="width: 400px; margin-top: 10px"
+      />
+      <a-button type="primary" @click="handleSubmit" style="margin-left: 10px">
+        确认
+      </a-button>
+
+      <div v-if="imageUrl" style="margin-top: 50px">
+        <img :src="imageUrl" alt="文生图" width="512" height="512" />
+      </div>
+    </a-card>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const models = [
+      { label: '模型1', value: 'model1' },
+      { label: '模型2', value: 'model2' }
+      // ...
+    ]
+
+    const subModels = [
+      { label: '子模型1', value: 'subModel1' },
+      { label: '子模型2', value: 'subModel2' }
+      // ...
+    ]
+
+    const selectedModel = ref(models[0].value)
+    const selectedSubModel = ref(subModels[0].value)
+    const keyword = ref('')
+    const imageUrl = ref('')
+
+    const handleSubmit = async () => {
+      if (!keyword.value) {
+        alert('请输入关键字')
+        return
+      }
+
+      try {
+        // 调用后台接口，根据用户输入的关键字和选择的模型、子模型获取图片链接
+        const response = await fetch('your-api-url', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            model: selectedModel.value,
+            subModel: selectedSubModel.value,
+            keyword: keyword.value
+          })
+        })
+
+        if (response.ok) {
+          const data = await response.json()
+          imageUrl.value = data.imageUrl
+        } else {
+          // alert('获取图片失败，请稍后重试')
+          imageUrl.value = 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+        }
+      } catch (error) {
+        console.error('Error fetching image:', error)
+        // alert('获取图片失败，请稍后重试')
+        imageUrl.value = 'https://p.qqan.com/up/2021-4/16196618195008329.jpg'
+      }
+    }
+
+    return {
+      models,
+      subModels,
+      selectedModel,
+      selectedSubModel,
+      keyword,
+      imageUrl,
+      handleSubmit
+    }
+  }
+}
+</script>
