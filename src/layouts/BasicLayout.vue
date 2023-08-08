55872e3ff49ca91bf6f1b1ce704ed9487154e120
bilibiliChangKai <3254266353@qq.com>
2023-08-09 00:10:39 +0800

    feat: 杂项处理 修改部分注释，添加logo文件

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -21,7 +21,7 @@
     -->
     <template v-slot:menuHeaderRender>
       <div>
-        <img src="@/assets/logo.svg" />
+        <img src="@/assets/bg.svg" />
         <h1>{{ title }}</h1>
       </div>
     </template>
--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -9,8 +9,8 @@
         <div class="top">
           <div class="header">
             <a href="/">
-              <img src="~@/assets/logo.svg" class="logo" alt="logo">
-              <span class="title">Ant Design</span>
+              <img src="~@/assets/bg.svg" class="logo" alt="logo">
+              <span class="title">万象绘</span>
             </a>
           </div>
           <div class="desc">
@@ -26,9 +26,9 @@
             <a href="_self">隐私</a>
             <a href="_self">条款</a>
           </div>
-          <div class="copyright">
+          <!-- <div class="copyright">
             Copyright &copy; 2018 vueComponent
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
